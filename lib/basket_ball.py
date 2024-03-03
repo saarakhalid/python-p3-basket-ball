afe9cc62724b1ef657eccca0be959c4ac4239352
SaaraKhalid <khalidsaara6@gmail.com>
2024-03-03 22:00:54 +0300

    completed assignment

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -1,3 +1,4 @@
+
 def game_dict():
     return {
         "home": {
@@ -182,3 +183,58 @@ def game_dict():
             ]
         }
     }
+
+def num_points_per_game(player_name):
+    game = game_dict()
+    for team in game.values():
+        for player in team["players"]:
+            if player["name"] == player_name:
+                return player["points_per_game"]
+
+def player_age(player_name):
+    game = game_dict()
+    for team in game.values():
+        for player in team["players"]:
+            if player["name"] == player_name:
+                return player["age"]
+
+def team_colors(team_name):
+    game = game_dict()
+    for team in game.values():
+        if team["team_name"] == team_name:
+            return team["colors"]
+
+def team_names():
+    game = game_dict()
+    return [team["team_name"] for team in game.values()]
+
+def player_numbers(team_name):
+    game = game_dict()
+    for team in game.values():
+        if team["team_name"] == team_name:
+            return [player["number"] for player in team["players"]]
+
+def player_stats(player_name):
+    game = game_dict()
+    for team in game.values():
+        for player in team["players"]:
+            if player["name"] == player_name:
+                return player
+
+def average_rebounds_by_shoe_brand():
+    game = game_dict()
+    shoe_brand_rebounds = {}
+    
+    for team in game.values():
+        for player in team["players"]:
+            brand = player["shoe_brand"]
+            rebounds = player["rebounds_per_game"]
+            if brand in shoe_brand_rebounds:
+                shoe_brand_rebounds[brand].append(rebounds)
+            else:
+                shoe_brand_rebounds[brand] = [rebounds]
+    
+    for brand, rebounds_list in shoe_brand_rebounds.items():
+        average_rebounds = sum(rebounds_list) / len(rebounds_list)
+        print(f"{brand}:  {average_rebounds:.2f}")
+
